77d517e17445c9adc1d4e4c8f34ccca869489c5f
Vicente Herrero (zeus) <viherbos@eln.upv.es>
2017-11-17 13:36:42 +0100

    1

--- open_data.py
+++ open_data.py
@@ -204,8 +204,8 @@ if __name__=="__main__":
     ts1 = np.array(DATA.loc[:,'timestamp1'])
     ts2 = np.array(DATA.loc[:,'timestamp2'])
 
-    fig=plt.figure(figsize=(12,4), hdf_filename)
-    fig.canvas.set_window_title()
+    fig=plt.figure(figsize=(12,4))
+    ##fig.canvas.set_window_title()
     gs = gridspec.GridSpec( nrows=4, ncols=4,
                             width_ratios=[2, 2, 2, 1])
     ax0 = fig.add_subplot(gs[:, 0])
